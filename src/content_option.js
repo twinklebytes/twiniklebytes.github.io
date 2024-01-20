a2499c7e81dad3aea7643590df823c89c8ee441a
Twinkle Bytes 👽 <twinklebytes@gmail.com>
2024-01-19 21:22:14 -0800

    Add portfolio

---
+++ src/content_option.js
@@ -0,0 +1,39 @@
+const logotext = "Gaurav";
+const meta = {
+    title: "Gaurav Sharma",
+    description: "I’m Gaurav Sharma computer scientist _ Full stack devloper,currently working in Seattle",
+};
+
+const introdata = {
+    title: "I’m Gaurav Sharma",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I'm deeply fascinated by technology and its vast realms. From computer science to the mysteries of space and rocket science, the exploration of geology, to the intricacies of physics, it's all exhilarating to me.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const contactConfig = {
+    YOUR_EMAIL: "twinklebytes@google.com",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
