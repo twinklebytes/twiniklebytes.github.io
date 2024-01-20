56e5c0e8aa53eb7c2c3c23ff9ea415d074c89065
Twinkle Bytes 👽 <twinklebytes@gmail.com>
2024-01-20 00:18:35 -0800

    Fixing the dark theme

--- src/header/index.js
+++ src/header/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./style.css";
 import { VscGrabber, VscClose } from "react-icons/vsc";
 import { Link } from "react-router-dom";
-import { logotext ,socialprofils } from "../content_option";
+import { logotext , socialprofils } from "../content_option";
 import Themetoggle from "../components/themetoggle";
 
 const Headermain = () => {
