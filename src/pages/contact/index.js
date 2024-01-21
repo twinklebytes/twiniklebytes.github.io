43de9bce94d387e79c73d3d82a0c7aa12037271a
Twinkle Bytes 👽 <twinklebytes@gmail.com>
2024-01-20 23:12:11 -0800

    Fixing success statement for emailjs

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -40,7 +40,7 @@ export const ContactUs = () => {
           console.log(result.text);
           setFormdata({
             loading: false,
-            alertmessage: "SUCCESS! ,Thankyou for your messege",
+            alertmessage: "Thank you for your message!",
             variant: "success",
             show: true,
           });
